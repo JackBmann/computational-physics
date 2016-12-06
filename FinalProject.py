123cb52b1443c1029871a74674e0c8e733a4c1d2
Jack Baumann <wavejbaumann@gmail.com>
2016-12-05 22:33:49 -0600

    Updated Final Project

--- FinalProject.py
+++ FinalProject.py
@@ -5,82 +5,76 @@ My final project analyzing the stock market with Chaos Theory
 """
 from math import sqrt, log, sin, pi
 import matplotlib.pyplot as plt
+from numpy import loadtxt
 
 
 def data(x):
-    return sin(x)
+    return prices[x-1]
+    # return (sin(2*pi*x/3) + 1)/2
 
 
 def x(tau):
-    rv = 0
+    sum = 0
     i = 1
     while i <= tau:
-    # for i in range(1, tau+1):
-    #     rv += xs[i]
-        rv += data(i)
+        sum += data(i)
         i += h
-    return 1.0/tau * rv
+    return 1.0/tau * sum
 
 
 def X(t, tau):
     rvs = []
+    # t = 0
     while t <= tau:
-    # for t in range(t, tau+1):
-        rv = 0
+        sum = 0
         i = 1
         while i <= t:
-        # for i in range(1, t+1):
-            # rv += xs[i] - x(tau)
-            rv += data(i) - x(tau)
+            sum += data(i) - x(tau)
             i += h
-        rvs.append(1.0/tau * rv)
+        rvs.append(sum)
         t += h
     return rvs
-    # rv = 0
-    # for i in range(1, t+1):
-    #     rv += xs[i] - x(tau)
-    # return 1.0/tau * rv
 
 
 def S(tau):
-    rv = 0
+    sum = 0
     i = 1
     while i <= tau:
-    # for i in range(1, tau+1):
-    #     rv += (xs[i] - x(tau))**2
-        rv += (data(i) - x(tau)) ** 2
+        sum += (data(i) - x(tau)) ** 2
         i += h
-    return sqrt(1.0/tau * rv)
+    return sqrt(1.0/tau * sum)
 
 
 def R(tau):
-    t = tau/2   # 1 <= t <= tau
+    # t = tau/2   # 1 <= t <= tau
     return max(X(t, tau)) - min(X(t, tau))
 
 
 def H(tau):
     c = 1.0     # constant that influences the value of H, default 1.0
+    if R(tau) == 0:
+        return 0
     return log(R(tau)/(S(tau)*c))/log(tau)
 
 
 def L(t):
     rv = 0
-    i = 0.1
+    i = 1
     while i <= t:
-    # for i in range(0, t):
-        rv += log(abs(data(i+h)/data(i)), 2)
-    #     rv += log(xs[i+1]/xs[i], 2)
+        if data(i) > 0 and data(i+h)/data(i) > 0:
+            rv += log(data(i+h)/data(i), 2)
         i += h
     return 1.0/t * rv
 
-t = 0.1
-tau = 2*pi
-h = 0.1
+t = 1
+tau = 250
+h = 1
 xs = []
 ts = []
 Hs = []
 Ls = []
 ps = []
+prices = loadtxt("Final Project Apple Stock Prices.csv", skiprows=1, delimiter=',')[:, 4]
 
 while t <= tau:
     xs.append(data(t))
@@ -90,13 +84,15 @@ while t <= tau:
     # ps.append(1.0/L(t))
     t += h
 
-print len(xs), len(ts), len(Hs)
 print ts
 print xs
 print Hs
 print Ls
+print ps
 # predictability = 1.0/L(t)
-plt.plot(ts, xs, 'k-')
-plt.plot(ts, Hs, 'b-')
-plt.plot(ts, Ls, 'r-')
+plt.plot(ts, xs, 'k-', label='Stock Price')
+plt.plot(ts, Hs, 'r-', label='Hurst Exponent')
+plt.plot(ts, Ls, 'b-', label='Lyapunov Exponent')
+# plt.plot(ts, ps, 'g-', label='Predictability')
+plt.legend(loc='best')
 plt.show()
